54f29cfad84d3590a1ab28ed32492a2c41baf70c
svifty7 <svifty7@gmail.com>
2022-11-05 19:32:32 +0300

    [FEATURE] D5-59: обновил ссылку на бота

--- src/store/UI/NavStore.js
+++ src/store/UI/NavStore.js
@@ -178,7 +178,7 @@ export const useNavStore = defineStore('NavStore', {
                         },
                         {
                             label: 'Наш бот для Telegram',
-                            url: 'https://t.me/dnd5club_bot',
+                            url: 'https://t.me/ttg_club_bot',
                             external: true
                         },
                         {
