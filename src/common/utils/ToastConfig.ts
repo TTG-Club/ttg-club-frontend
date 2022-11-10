132d7ddb11d62b77107ee3c614ecec6a1d4f5e05
svifty7 <svifty7@gmail.com>
2022-11-11 02:49:43 +0300

    D5-52: update toast

--- src/common/utils/ToastConfig.ts
+++ src/common/utils/ToastConfig.ts
@@ -1,20 +1,42 @@
 import type { PluginOptions, ToastOptionsAndRequiredContent } from 'vue-toastification/dist/types/types';
-import { EventBus } from 'vue-toastification';
+import { EventBus, useToast } from 'vue-toastification';
+import orderBy from 'lodash/orderBy';
 import IconToastClose from '@/components/UI/icons/IconToastClose.vue';
 
 export const ToastEventBus = new EventBus();
 
+const maxToasts = 6;
+
 const filterToasts = (toasts: Array<ToastOptionsAndRequiredContent>): Array<ToastOptionsAndRequiredContent> => toasts;
+
+const filterBeforeCreate = (
+    toast: ToastOptionsAndRequiredContent,
+    toasts: Array<ToastOptionsAndRequiredContent>
+): ToastOptionsAndRequiredContent | false => {
+    if (maxToasts > toasts.length) {
+        return toast;
+    }
+
+    const toastsForDelete = orderBy(toasts, ['id']).slice(0, Math.abs(maxToasts - toasts.length + 1));
+    const toastComposition = useToast(ToastEventBus);
+
+    for (const toastForDelete of toastsForDelete) {
+        toastComposition.dismiss(toastForDelete.id!);
+    }
+
+    return toast;
+};
 
 export const ToastOptions: PluginOptions = {
     shareAppContext: true,
     timeout: 1700,
     closeButton: IconToastClose,
     showCloseButtonOnHover: true,
-    maxToasts: 6,
     newestOnTop: true,
     eventBus: ToastEventBus,
-    filterToasts
+    filterBeforeCreate,
+    filterToasts,
+    maxToasts
 };
 
 export default {
