5fc6dc1a0e35a2824328a66acc31dcb747f901e7
Peterko <peterko@gmail.com>
2022-11-29 11:40:50 +0300

    Fix bild

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 const path = require('path');
 
 module.exports = defineConfig({
-    outputDir: process.env.BUILD_PATH || './dist/',
+    outputDir: process.env.VUE_APP_BUILD_PATH || './dist/',
     filenameHashing: false,
     runtimeCompiler: true,
     productionSourceMap: true,
