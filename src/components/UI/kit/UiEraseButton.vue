910cc520564e8b12984d021ed85cc5e0a992f32f
FiNaLiZee <sd103e@mail.ru>
2023-05-01 23:16:51 +0300

    fix: erase-btn script to setup; styles spaces

--- src/components/UI/kit/UiEraseButton.vue
+++ src/components/UI/kit/UiEraseButton.vue
@@ -9,39 +9,32 @@
     </button>
 </template>
 
-<script>
-    import { defineComponent } from 'vue';
+<script setup lang="ts">
     import SvgIcon from "@/components/UI/icons/SvgIcon.vue";
-
-    export default defineComponent({
-        components: {
-            SvgIcon
-        }
-    });
 </script>
 
 <style lang="scss" scoped>
-.filter__search_clear {
-    @include css_anim();
+    .filter__search_clear {
+        @include css_anim();
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 42px;
-    height: 42px;
-    flex-shrink: 0;
-    color: var(--primary);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 42px;
+        height: 42px;
+        flex-shrink: 0;
+        color: var(--primary);
 
-    @include media-min($md) {
-        &:hover {
-            color: var(--text-btn-color);
-            background-color: var(--primary-hover);
+        @include media-min($md) {
+            &:hover {
+                color: var(--text-btn-color);
+                background-color: var(--primary-hover);
+            }
+        }
+
+        svg {
+            width: 16px;
+            height: 16px;
         }
     }
-
-    svg {
-        width: 16px;
-        height: 16px;
-    }
-}
 </style>
