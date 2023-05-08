14ca1121fb3f6df4689ee2eda30b1312e667767a
Alexey Troshin <alexey.troshin.dev@gmail.com>
2023-05-08 14:43:28 +0300

    refactor(ItemsView): add VirtualGridList, script setup

--- src/views/Inventory/Items/ItemsView.vue
+++ src/views/Inventory/Items/ItemsView.vue
@@ -7,95 +7,82 @@
         @update="initPages"
         @list-end="nextPage"
     >
-        <item-link
-            v-for="item in items"
-            :key="item.url"
-            :item="item"
-            :to="{ path: item.url }"
-        />
+        <virtual-grid-list
+            :list="{ items, keyField: 'url' }"
+            :flat="showRightSide"
+        >
+            <template #default="{ item }">
+                <item-link
+                    :item="item"
+                    :to="{ path: item.url }"
+                />
+            </template>
+        </virtual-grid-list>
     </content-layout>
 </template>
 
-<script>
-    import {
-        computed, defineComponent, onBeforeMount
-    } from 'vue';
+<script lang="ts" setup>
     import { storeToRefs } from 'pinia';
+    import { computed, onBeforeMount } from 'vue';
     import { useRoute, useRouter } from 'vue-router';
-    import ContentLayout from '@/components/content/ContentLayout.vue';
-    import ItemLink from '@/views/Inventory/Items/ItemLink.vue';
-    import { useUIStore } from '@/store/UI/UIStore';
     import { useFilter } from '@/common/composition/useFilter';
-    import { ItemsFilterDefaults } from '@/types/Inventory/Items.types';
     import { usePagination } from '@/common/composition/usePagination';
+    import ContentLayout from '@/components/content/ContentLayout.vue';
+    import VirtualGridList from '@/components/list/VirtualGridList/VirtualGridList.vue';
+    import { useUIStore } from '@/store/UI/UIStore';
+    import { ItemsFilterDefaults } from '@/types/Inventory/Items.types';
+    import ItemLink from '@/views/Inventory/Items/ItemLink.vue';
 
-    export default defineComponent({
-        components: {
-            ItemLink,
-            ContentLayout
+    const route = useRoute();
+    const router = useRouter();
+    const uiStore = useUIStore();
+
+    const {
+        isMobile,
+        fullscreen
+    } = storeToRefs(uiStore);
+
+    const filter = useFilter({
+        dbName: ItemsFilterDefaults.dbName,
+        url: ItemsFilterDefaults.url
+    });
+
+    const {
+        initPages,
+        nextPage,
+        items
+    } = usePagination({
+        url: '/items',
+        limit: 70,
+        filter: {
+            isCustomized: filter.isCustomized,
+            value: filter.queryParams
         },
-        setup() {
-            const route = useRoute();
-            const router = useRouter();
-            const uiStore = useUIStore();
+        search: filter.search,
+        order: [
+            {
+                field: 'name',
+                direction: 'asc'
+            }
+        ]
+    });
 
-            const {
-                isMobile,
-                fullscreen
-            } = storeToRefs(uiStore);
+    const onSearch = async () => {
+        await initPages();
 
-            const filter = useFilter({
-                dbName: ItemsFilterDefaults.dbName,
-                url: ItemsFilterDefaults.url
-            });
+        if (items.value.length === 1 && !isMobile.value) {
+            await router.push({ path: items.value[0].url });
+        }
+    };
 
-            const {
-                initPages,
-                nextPage,
-                items
-            } = usePagination({
-                url: '/items',
-                limit: 70,
-                filter: {
-                    isCustomized: filter.isCustomized,
-                    value: filter.queryParams
-                },
-                search: filter.search,
-                order: [
-                    {
-                        field: 'name',
-                        direction: 'asc'
-                    }
-                ]
-            });
+    onBeforeMount(async () => {
+        await filter.initFilter();
+        await initPages();
 
-            const onSearch = async () => {
-                await initPages();
-
-                if (items.value.length === 1 && !isMobile.value) {
-                    await router.push({ path: items.value[0].url });
-                }
-            };
-
-            onBeforeMount(async () => {
-                await filter.initFilter();
-                await initPages();
-
-                if (!isMobile.value && items.value.length && route.name === 'items') {
-                    await router.push({ path: items.value[0].url });
-                }
-            });
-
-            return {
-                isMobile,
-                fullscreen,
-                items,
-                filter,
-                showRightSide: computed(() => route.name === 'itemDetail'),
-                initPages,
-                nextPage,
-                onSearch
-            };
+        if (!isMobile.value && items.value.length && route.name === 'items') {
+            await router.push({ path: items.value[0].url });
         }
     });
+
+    const showRightSide = computed(() => route.name === 'itemDetail');
 </script>
