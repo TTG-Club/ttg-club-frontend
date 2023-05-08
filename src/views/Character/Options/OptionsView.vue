d5ffb7788529c542f33961a6a1b9931bc58e51ac
Alexey Troshin <alexey.troshin.dev@gmail.com>
2023-05-08 14:19:12 +0300

    fix(OptionsView): fix options view not loading items, add VirtualGridList component

--- src/views/Character/Options/OptionsView.vue
+++ src/views/Character/Options/OptionsView.vue
@@ -6,21 +6,26 @@
         :show-right-side="showRightSide"
         @search="onSearch"
         @update="initPages"
+        @list-end="nextPage"
     >
-        <option-link
-            v-for="option in options"
-            :key="option.url"
-            :in-tab="inTab"
-            :option-item="option"
-            :to="{ path: option.url }"
-        />
+        <virtual-grid-list
+            :list="{ items: options, keyField: 'url' }"
+            :flat="showRightSide"
+        >
+            <template #default="{ item: option }">
+                <option-link
+                    :in-tab="inTab"
+                    :option-item="option"
+                    :to="{ path: option.url }"
+                />
+            </template>
+        </virtual-grid-list>
     </component>
 </template>
 
-<script lang="ts">
-    import type { PropType } from 'vue';
+<script lang="ts" setup>
     import {
-        computed, defineComponent, onBeforeMount, watch
+        computed, onBeforeMount, watch
     } from 'vue';
     import { storeToRefs } from 'pinia';
     import { useRoute, useRouter } from 'vue-router';
@@ -31,129 +36,109 @@
     import { useFilter } from '@/common/composition/useFilter';
     import { usePagination } from '@/common/composition/usePagination';
     import { OptionsFilterDefaults } from '@/types/Character/Options.types';
+    import VirtualGridList from "@/components/list/VirtualGridList/VirtualGridList.vue";
 
-    export default defineComponent({
-        components: {
-            OptionLink,
-            TabLayout,
-            ContentLayout
-        },
-        props: {
-            inTab: {
-                type: Boolean,
-                default: false
-            },
-            storeKey: {
-                type: String,
-                default: ''
-            },
-            filterUrl: {
-                type: String,
-                default: undefined
-            },
-            queryBooks: {
-                type: Array as PropType<Array<string>>,
-                default: undefined
-            }
-        },
-        setup(props) {
-            const route = useRoute();
-            const router = useRouter();
-            const uiStore = useUIStore();
+    type TProps = {
+        inTab?: boolean,
+        storeKey?: string,
+        filterUrl?: string,
+        queryBooks?: string[],
+    };
 
-            const {
-                isMobile,
-                fullscreen
-            } = storeToRefs(uiStore);
+    const props = withDefaults(defineProps<TProps>(), {
+        inTab: false,
+        storeKey: '',
+        filterUrl: undefined,
+        queryBooks: undefined
+    });
 
-            const layout = computed(() => (
-                props.inTab
-                    ? TabLayout
-                    : ContentLayout
-            ));
+    const route = useRoute();
+    const router = useRouter();
+    const uiStore = useUIStore();
 
-            const filter = useFilter({
-                dbName: OptionsFilterDefaults.dbName,
-                storeKey: computed(() => props.storeKey),
-                url: computed(() => props.filterUrl || OptionsFilterDefaults.url)
-            });
+    const {
+        isMobile,
+        fullscreen
+    } = storeToRefs(uiStore);
 
-            const isCustomized = computed(() => !!props.queryBooks || filter.isCustomized.value);
+    const layout = computed(() => (
+        props.inTab
+            ? TabLayout
+            : ContentLayout
+    ));
 
-            const queryParams = computed(() => {
-                if (props.queryBooks) {
-                    return {
-                        ...filter.queryParams.value,
-                        book: props.queryBooks
-                    };
-                }
+    const filter = useFilter({
+        dbName: OptionsFilterDefaults.dbName,
+        storeKey: computed(() => props.storeKey),
+        url: computed(() => props.filterUrl || OptionsFilterDefaults.url)
+    });
 
-                return filter.queryParams.value;
-            });
+    const isCustomized = computed(() => !!props.queryBooks || filter.isCustomized.value);
 
-            const {
-                initPages,
-                resetPages,
-                items: options
-            } = usePagination({
-                url: '/options',
-                limit: 70,
-                filter: {
-                    isCustomized,
-                    value: queryParams
-                },
-                search: filter.search,
-                order: [
-                    {
-                        field: 'name',
-                        direction: 'asc'
-                    }
-                ]
-            });
-
-            const onSearch = async () => {
-                await initPages();
-
-                if (options.value.length === 1 && !isMobile.value) {
-                    await router.push({ path: options.value[0].url });
-                }
-            };
-
-            onBeforeMount(async () => {
-                await filter.initFilter();
-                await initPages();
-
-                if (!isMobile.value && options.value.length && route.name === 'options') {
-                    await router.push({ path: options.value[0].url });
-                }
-            });
-
-            watch(
-                [
-                    () => props.queryBooks,
-                    () => props.filterUrl,
-                    () => props.storeKey
-                ],
-                async () => {
-                    await resetPages();
-                    await filter.initFilter();
-                    await initPages();
-                },
-                {
-                    deep: true
-                }
-            );
-
+    const queryParams = computed(() => {
+        if (props.queryBooks) {
             return {
-                layout,
-                isMobile,
-                fullscreen,
-                options,
-                filter,
-                showRightSide: computed(() => route.name === 'optionDetail'),
-                initPages,
-                onSearch
+                ...filter.queryParams.value,
+                book: props.queryBooks
             };
         }
+
+        return filter.queryParams.value;
     });
+
+    const {
+        initPages,
+        nextPage,
+        resetPages,
+        items: options
+    } = usePagination({
+        url: '/options',
+        limit: 70,
+        filter: {
+            isCustomized,
+            value: queryParams
+        },
+        search: filter.search,
+        order: [
+            {
+                field: 'name',
+                direction: 'asc'
+            }
+        ]
+    });
+
+    const onSearch = async () => {
+        await initPages();
+
+        if (options.value.length === 1 && !isMobile.value) {
+            await router.push({ path: options.value[0].url });
+        }
+    };
+
+    onBeforeMount(async () => {
+        await filter.initFilter();
+        await initPages();
+
+        if (!isMobile.value && options.value.length && route.name === 'options') {
+            await router.push({ path: options.value[0].url });
+        }
+    });
+
+    watch(
+        [
+            () => props.queryBooks,
+            () => props.filterUrl,
+            () => props.storeKey
+        ],
+        async () => {
+            await resetPages();
+            await filter.initFilter();
+            await initPages();
+        },
+        {
+            deep: true
+        }
+    );
+
+    const showRightSide = computed(() => route.name === 'optionDetail');
 </script>
