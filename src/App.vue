bfef79b13b3c0427aefefba81315a149371806c5
svifty7 <svifty7@gmail.com>
2023-05-13 08:33:57 +0300

    feat(BugReporter): disable reporter on mobile

--- src/App.vue
+++ src/App.vue
@@ -10,59 +10,52 @@
 
     <modals-container />
 
-    <bug-reporter />
+    <bug-reporter v-if="!isMobile" />
 </template>
 
-<script lang="ts">
-    import { defineComponent } from 'vue';
+<script setup lang="ts">
     import { tryOnBeforeMount } from '@vueuse/core';
     import { ModalsContainer } from 'vue-final-modal';
+    import { storeToRefs } from 'pinia';
     import { useUIStore } from '@/store/UI/UIStore';
     import { useUserStore } from '@/store/UI/UserStore';
     import NavBar from '@/components/UI/menu/NavBar.vue';
     import BugReporter from '@/components/UI/BugReporter.vue';
 
-    export default defineComponent({
-        components: {
-            ModalsContainer,
-            BugReporter,
-            NavBar
-        },
-        setup() {
-            const uiStore = useUIStore();
-            const userStore = useUserStore();
+    const uiStore = useUIStore();
+    const userStore = useUserStore();
 
-            const initUser = async () => {
-                try {
-                    if (await userStore.getUserStatus()) {
-                        await userStore.getUserInfo();
-                    }
-                } catch (err) {
-                    await userStore.clearUser();
-                }
-            };
+    const { isMobile } = storeToRefs(uiStore);
 
-            const initTheme = async () => {
-                await uiStore.removeOldTheme();
-
-                const html = document.querySelector('html');
-
-                let avoidHtmlUpdate = false;
-
-                if (html?.dataset?.theme) {
-                    avoidHtmlUpdate = ['theme-light', 'theme-dark'].includes(html?.dataset?.theme);
-                }
-
-                uiStore.setTheme({
-                    name: uiStore.getCookieTheme(),
-                    avoidHtmlUpdate
-                });
-            };
-
-            tryOnBeforeMount(async () => {
-                await initTheme();
-                await initUser();
-            });
+    const initUser = async () => {
+        try {
+            if (await userStore.getUserStatus()) {
+                await userStore.getUserInfo();
+            }
+        } catch (err) {
+            await userStore.clearUser();
         }
+    };
+
+    const initTheme = async () => {
+        await uiStore.removeOldTheme();
+
+        const html = document.querySelector('html');
+
+        let avoidHtmlUpdate = false;
+
+        if (html?.dataset?.theme) {
+            avoidHtmlUpdate = ['theme-light', 'theme-dark'].includes(html?.dataset?.theme);
+        }
+
+        uiStore.setTheme({
+            name: uiStore.getCookieTheme(),
+            avoidHtmlUpdate
+        });
+    };
+
+    tryOnBeforeMount(async () => {
+        await initTheme();
+        await initUser();
     });
 </script>
