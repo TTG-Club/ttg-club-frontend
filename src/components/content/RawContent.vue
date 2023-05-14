8270cb8693167ac2fe93cb47f3c14cf43f472f1e
svifty7 <svifty7@gmail.com>
2023-05-14 20:44:03 +0300

    chore: resolve conflicts

--- src/components/content/RawContent.vue
+++ src/components/content/RawContent.vue
@@ -1,85 +1,85 @@
 <template>
-    <render />
+  <render />
 </template>
 
 <script lang="ts" setup>
-    import {
-        computed, defineComponent, h, onBeforeUnmount, ref, watch
-    } from 'vue';
-    import { useAxios } from '@/common/composition/useAxios';
+  import {
+    computed, defineComponent, h, onBeforeUnmount, ref, watch
+  } from 'vue';
+  import { useAxios } from '@/common/composition/useAxios';
 
-    const props = withDefaults(defineProps<{
-        template?: string;
-        url?: string;
-        tag?: string;
-    }>(), {
-        template: undefined,
-        url: undefined,
-        tag: 'div'
-    });
+  const props = withDefaults(defineProps<{
+    template?: string;
+    url?: string;
+    tag?: string;
+  }>(), {
+    template: undefined,
+    url: undefined,
+    tag: 'div'
+  });
 
-    const emit = defineEmits(['loaded', 'before-unmount']);
+  const emit = defineEmits(['loaded', 'before-unmount']);
 
-    const http = useAxios();
-    const templateString = ref<string | null>(null);
-    const error = ref(false);
-    const loading = ref(true);
+  const http = useAxios();
+  const templateString = ref<string | null>(null);
+  const error = ref(false);
+  const loading = ref(true);
 
-    const updateTemplate = async () => {
-        if (!props.template && !props.url) {
-            throw new Error('URL and template is not defined');
-        }
+  const updateTemplate = async () => {
+    if (!props.template && !props.url) {
+      throw new Error('URL and template is not defined');
+    }
 
-        try {
-            error.value = false;
-            loading.value = true;
+    try {
+      error.value = false;
+      loading.value = true;
 
-            templateString.value = props.template || '';
+      templateString.value = props.template || '';
 
-            if (!templateString.value && !props.template) {
-                const { data } = await http.rawGet({
-                    url: props.url
-                });
+      if (!templateString.value && !props.template) {
+        const { data } = await http.rawGet({
+          url: props.url
+        });
 
-                templateString.value = data;
-            }
+        templateString.value = data;
+      }
 
-            loading.value = false;
+      loading.value = false;
 
-            emit('loaded');
+      emit('loaded');
 
-            return Promise.resolve();
-        } catch (err) {
-            error.value = true;
-            loading.value = false;
+      return Promise.resolve();
+    } catch (err) {
+      error.value = true;
+      loading.value = false;
 
-            return Promise.reject(err);
-        }
-    };
+      return Promise.reject(err);
+    }
+  };
 
-    const render = computed(() => {
-        if (loading.value) {
-            return h(props.tag, null, 'Загрузка...');
-        }
+  const render = computed(() => {
+    if (loading.value) {
+      return h(props.tag, null, 'Загрузка...');
+    }
 
-        if (error.value || !templateString.value) {
-            return h(props.tag, null, 'Ошибка...');
-        }
+    if (error.value || !templateString.value) {
+      return h(props.tag, null, 'Ошибка...');
+    }
 
-        return h(props.tag, null, h(defineComponent({ template: templateString.value })));
-    });
+    return h(props.tag, null, h(defineComponent({ template: templateString.value })));
+  });
 
-    watch(
-        [() => props.template, () => props.url],
-        async () => {
-            await updateTemplate();
-        },
-        {
-            immediate: true
-        }
-    );
+  watch(
+    [() => props.template, () => props.url],
+    async () => {
+      await updateTemplate();
+    },
+    {
+      immediate: true
+    }
+  );
 
-    onBeforeUnmount(() => {
-        emit('before-unmount');
-    });
+  onBeforeUnmount(() => {
+    emit('before-unmount');
+  });
 </script>
