61e0f17d05c0d4fa0faa59afae454609ce908e6b
Peterko <peterko@gmail.com>
2023-05-22 16:00:46 +0300

    fix(index.ts): default limit 160

--- src/common/const/index.ts
+++ src/common/const/index.ts
@@ -4,4 +4,4 @@ export const DEFAULT_KEY_FIELD = 'id';
 
 export const DEFAULT_ENTITY_KEY_FIELD = 'url';
 
-export const DEFAULT_PAGINATION_ITEMS_LIMIT = 120;
+export const DEFAULT_PAGINATION_ITEMS_LIMIT = 160;
