056e00b9426a77a946e2e6929eb51081477cb1fa
FiNaLiZee <sd103e@mail.ru>
2023-05-26 21:28:21 +0300

    hotfix(CreatureBody): remove getFormattedModifier from skills and savingThrows

--- src/views/Workshop/Bestiary/CreatureBody.vue
+++ src/views/Workshop/Bestiary/CreatureBody.vue
@@ -482,11 +482,13 @@
       return [];
     }
 
+    console.log(props.creature.savingThrows);
+
     return props.creature.savingThrows.map(save => ({
-      formula: `к20${ getFormattedModifier(save.value) }`,
+      formula: `к20+${ save.value }`,
       label: save.shortName,
       name: save.name,
-      value: `${ getFormattedModifier(save.value) }${ save.additional ? save.additional : '' }`
+      value: `+${ save.value }${ save.additional ? save.additional : '' }`
     }));
   });
 
@@ -496,9 +498,9 @@
     }
 
     return props.creature.skills.map(skill => ({
-      formula: `к20${ getFormattedModifier(skill.value) }`,
+      formula: `к20+${ skills.value }`,
       label: skill.name,
-      value: `${ getFormattedModifier(skill.value) }${ skill.additional ? skill.additional : '' }`
+      value: `+${ skill.value }${ skill.additional ? skill.additional : '' }`
     }));
   });
 
