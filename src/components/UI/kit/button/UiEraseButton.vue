27a1b5ccf358c6e2f6e58e86b14c277454004f13
Alexey Troshin <alexey.troshin.dev@gmail.com>
2023-06-14 13:49:04 +0300

    fix(UiInput): fix erase button stretching box height

--- src/components/UI/kit/button/UiEraseButton.vue
+++ src/components/UI/kit/button/UiEraseButton.vue
@@ -2,6 +2,7 @@
   <ui-button
     v-tippy="{ content: 'Стереть строку поиска' }"
     :class="$style['ui-erase-button']"
+    :body-class="bodyClasses"
     type="text"
     icon="close"
     color="text"
@@ -11,19 +12,26 @@
 
 <script setup lang="ts">
   import { useVModel } from '@vueuse/core';
-  import SvgIcon from '@/components/UI/icons/SvgIcon.vue';
+  import { useCssModule } from 'vue';
+  import clsx from 'clsx';
   import UiButton from '@/components/UI/kit/button/UiButton.vue';
+  import type { ClassName } from '@/types/Shared/Vue.types';
 
   const props = defineProps<{
     modelValue: string;
+    bodyClass: ClassName;
   }>();
+
+  const $style = useCssModule();
+
+  const bodyClasses = clsx([props.bodyClass, $style.body]);
 
   const value = useVModel(props, 'modelValue');
 </script>
 
 <style lang="scss" module>
   .ui-erase-button {
-    button {
+    .body {
       height: 100%;
       border-radius: 0;
     }
