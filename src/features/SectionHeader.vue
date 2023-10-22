315bbddef5aa9a2d840186eae0235256e735f36d
Oleh-Volkov_ghub <Oleh.Volkov@blueyonder.com>
2023-10-22 17:06:12 +0300

    remove extra prop

--- src/features/SectionHeader.vue
+++ src/features/SectionHeader.vue
@@ -54,8 +54,7 @@
         v-if="onExportFoundry"
         :tooltip="{
           content:
-            '<span>Импорт в Foundry VTT.&nbsp;<a href=&#34;/info/fvtt_import&#34; target=&#34;_blank&#34;>Инструкция</a>',
-          hideOnClick: true
+            '<span>Импорт в Foundry VTT.&nbsp;<a href=&#34;/info/fvtt_import&#34; target=&#34;_blank&#34;>Инструкция</a>'
         }"
         class="section-header__control is-only-desktop"
         icon="export-foundry"
