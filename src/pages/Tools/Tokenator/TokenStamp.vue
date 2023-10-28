3213f972867018e4d2a9956451aafd01e67869b1
Oleg Volkov <voa91ua@gmail.com>
2023-10-28 18:03:01 +0300

    update token stamp logic to render layout between border and background

--- src/pages/Tools/Tokenator/TokenStamp.vue
+++ src/pages/Tools/Tokenator/TokenStamp.vue
@@ -12,11 +12,11 @@
   import { ref, onMounted, computed, watch } from 'vue';
 
   import backgorundImage from '@/pages/Tools/Tokenator/assets/token-bg.webp';
-  import defaultImage from '@/pages/Tools/Tokenator/assets/token-border.webp';
+  import borderImage from '@/pages/Tools/Tokenator/assets/token-border.webp';
 
   const canvasRef = ref<HTMLCanvasElement | null>(null);
   const contextRef = ref<CanvasRenderingContext2D | null>(null);
-  const defaultImageRef = ref(new Image());
+  const borderImageRef = ref(new Image());
   const bgImageRef = ref(new Image());
 
   const props = defineProps<{
@@ -27,7 +27,7 @@
 
   const scaleToFitFactor = computed(() => {
     const canvas = canvasRef.value;
-    const image = defaultImageRef.value;
+    const image = borderImageRef.value;
 
     if (canvas) {
       return Math.min(canvas.width / image.width, canvas.height / image.height);
@@ -42,7 +42,7 @@
     if (canvas) {
       const context = canvas.getContext('2d');
 
-      defaultImageRef.value.src = defaultImage;
+      borderImageRef.value.src = borderImage;
       bgImageRef.value.src = backgorundImage;
 
       Promise.all([
@@ -50,15 +50,15 @@
           bgImageRef.value.onload = resolve;
         }),
         new Promise(resolve => {
-          defaultImageRef.value.onload = resolve;
+          borderImageRef.value.onload = resolve;
         })
       ])
         .then(() => {
-          const scale = scaleToFitFactor.value;
+          const scaleFactor = scaleToFitFactor.value;
 
           // так как размеры порядочные, нет смысла считать тоже самое для
-          const newWidth = defaultImageRef.value.width * scale;
-          const newHeight = defaultImageRef.value.height * scale;
+          const newWidth = borderImageRef.value.width * scaleFactor;
+          const newHeight = borderImageRef.value.height * scaleFactor;
 
           const x = (canvas.width - newWidth) / 2;
           const y = (canvas.height - newHeight) / 2;
@@ -79,7 +79,7 @@
             context.drawImage(bgImageRef.value, x, y, newWidth, newHeight);
             context.restore();
 
-            context.drawImage(defaultImageRef.value, x, y, newWidth, newHeight);
+            context.drawImage(borderImageRef.value, x, y, newWidth, newHeight);
             contextRef.value = context;
           }
         })
@@ -89,6 +89,35 @@
         });
     }
   });
+
+  const drawImage = (image, scale) => {
+    const canvas = canvasRef.value;
+    const context = contextRef.value;
+
+    context.save();
+    context.beginPath();
+
+    context.arc(
+      canvas.width / 2,
+      canvas.height / 2,
+      canvas.width / 2.4,
+      0,
+      Math.PI * 2
+    );
+    context.clip();
+
+    const newWidth = image.width * scale;
+    const newHeight = image.height * scale;
+
+    const x = (canvas.width - newWidth) / 2;
+    const y = (canvas.height - newHeight) / 2;
+
+    context.clearRect(0, 0, canvas?.width, canvas.height);
+
+    context.drawImage(bgImageRef.value, 0, 0, canvas?.width, canvas?.height);
+    context.drawImage(image, x, y, image.width * scale, image.height * scale);
+    context.restore();
+  };
 
   watch([() => props.source, () => props.scale], ([source, scale]) => {
     console.log('test', source);
@@ -102,16 +131,48 @@
       console.log('img.src', img.src);
 
       img.onload = () => {
-        if (canvasRef.value) {
-          const newWidth = canvasRef.value.width * scale;
-          const newHeight = canvasRef.value.height * scale;
+        const canvas = canvasRef.value;
 
-          const x = (canvasRef.value.width - newWidth) / 2;
-          const y = (canvasRef.value.height - newHeight) / 2;
+        if (canvas) {
+          const context = contextRef.value;
 
-          if (contextRef.value) {
-            contextRef.value.drawImage(img, x, y, newWidth, newHeight);
-          }
+          borderImageRef.value.src = borderImage;
+          bgImageRef.value.src = backgorundImage;
+
+          Promise.all([
+            new Promise(resolve => {
+              bgImageRef.value.onload = resolve;
+            }),
+            new Promise(resolve => {
+              borderImageRef.value.onload = resolve;
+            })
+          ])
+            .then(() => {
+              const scaleFactor = scaleToFitFactor.value;
+
+              // так как размеры порядочные, нет смысла считать тоже самое для
+              const newWidth = borderImageRef.value.width * scaleFactor;
+              const newHeight = borderImageRef.value.height * scaleFactor;
+
+              const x = (canvas.width - newWidth) / 2;
+              const y = (canvas.height - newHeight) / 2;
+
+              if (context) {
+                drawImage(img, scale);
+
+                context.drawImage(
+                  borderImageRef.value,
+                  x,
+                  y,
+                  newWidth,
+                  newHeight
+                );
+              }
+            })
+            .catch(err => {
+              // думаю здесь можно toast добавить
+              console.log('ЧТО-ТО ПОШЛО НЕ ТАК', err);
+            });
         }
       };
     }
