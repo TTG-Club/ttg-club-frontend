d04195e056a78c8be404c7407c4e1847c5eb0a9c
Alexey Kunitsky <kunitsky.alexey@gmail.com>
2023-10-28 20:52:47 +0200

    feat: implement hover styles for chips

--- src/shared/ui/kit/chip/UiChip.vue
+++ src/shared/ui/kit/chip/UiChip.vue
@@ -37,13 +37,43 @@
 </script>
 
 <style lang="scss" module>
+  @use 'src/assets/styles/variables/mixins' as mixins;
+
   .chip {
     background-color: var(--primary);
     color: var(--text-btn-color);
     font-weight: 400;
+    position: relative;
+    overflow: hidden;
 
     &.is-active {
       background-color: var(--primary);
+    }
+
+    // before for hover, after for disabled
+    &::after,
+    &::before {
+      @include mixins.css_anim($item: opacity);
+
+      content: '';
+      display: block;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      left: 0;
+      top: 0;
+    }
+
+    &::before {
+      background-color: black;
+      opacity: 0;
+      z-index: 1;
+    }
+
+    &:hover {
+      &::before {
+        opacity: 0.15;
+      }
     }
   }
 
@@ -86,5 +116,10 @@
     &-rectangle {
       border-radius: 8px;
     }
+  }
+
+  .caption {
+    position: relative;
+    z-index: 2;
   }
 </style>
