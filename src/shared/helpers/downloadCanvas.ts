7a17486f95d09a3eb497a406acce22cb37319290
Oleg Volkov <voa91ua@gmail.com>
2023-10-28 13:59:45 +0300

    refactor download canvas method

---
+++ src/shared/helpers/downloadCanvas.ts
@@ -0,0 +1,16 @@
+export const downloadCanvas = () => {
+  const canvas = document.getElementById('canvasToken') as HTMLCanvasElement;
+
+  if (canvas) {
+    const canvasDataUrl = canvas.toDataURL('image/webp');
+
+    const a = document.createElement('a');
+
+    a.href = canvasDataUrl;
+    a.download = 'token.webp';
+    a.style.display = 'none';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }
+};
