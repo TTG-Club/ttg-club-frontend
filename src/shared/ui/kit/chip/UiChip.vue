bca5ec93f5bb62a37cb4aa8aed63089b3ba021c4
Alexey Kunitsky <kunitsky.alexey@gmail.com>
2023-10-28 20:56:11 +0200

    feat: rework hover using vars

--- src/shared/ui/kit/chip/UiChip.vue
+++ src/shared/ui/kit/chip/UiChip.vue
@@ -40,6 +40,8 @@
   @use 'src/assets/styles/variables/mixins' as mixins;
 
   .chip {
+    @include mixins.css_anim($item: background-color);
+
     background-color: var(--primary);
     color: var(--text-btn-color);
     font-weight: 400;
@@ -50,30 +52,8 @@
       background-color: var(--primary);
     }
 
-    // before for hover, after for disabled
-    &::after,
-    &::before {
-      @include mixins.css_anim($item: opacity);
-
-      content: '';
-      display: block;
-      position: absolute;
-      width: 100%;
-      height: 100%;
-      left: 0;
-      top: 0;
-    }
-
-    &::before {
-      background-color: black;
-      opacity: 0;
-      z-index: 1;
-    }
-
     &:hover {
-      &::before {
-        opacity: 0.15;
-      }
+      background-color: var(--primary-hover);
     }
   }
 
