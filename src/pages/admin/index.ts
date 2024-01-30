39025bb23bf03b03f32c717c5a7cb25cc151bcce
svifty7 <svifty7@gmail.com>
2024-01-30 09:38:52 +0300

    fix: admin section auth

--- src/pages/admin/index.ts
+++ src/pages/admin/index.ts
@@ -1,3 +1,5 @@
+import { EUserRoles, useUserStore } from '@/shared/stores/UserStore';
+
 import { IconsPreviewPage } from './icons-preview';
 import { AdminYoutubePage } from './youtube';
 
@@ -6,5 +8,27 @@ import type { RouteRecordRaw } from 'vue-router';
 export const AdminPage: RouteRecordRaw = {
   name: 'admin',
   path: '/admin',
-  children: [AdminYoutubePage, IconsPreviewPage]
+  children: [AdminYoutubePage, IconsPreviewPage],
+  beforeEnter: async () => {
+    const userStore = useUserStore();
+
+    try {
+      const user = await userStore.getUserInfo();
+
+      if (!user) {
+        return { name: 'unauthorized' };
+      }
+
+      if (
+        user.roles.includes(EUserRoles.MODERATOR) ||
+        user.roles.includes(EUserRoles.ADMIN)
+      ) {
+        return true;
+      }
+
+      return { name: 'forbidden' };
+    } catch (err) {
+      return { name: 'internal-server' };
+    }
+  }
 };
