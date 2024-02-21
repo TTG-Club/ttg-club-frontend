d5f836dfa83e26c708a81697966567fbca72f6b2
Peterko <peterko@gmail.com>
2024-02-21 13:33:57 +0300

    fix

--- src/pages/tools/ability-calc/AbilityTable.vue
+++ src/pages/tools/ability-calc/AbilityTable.vue
@@ -36,9 +36,8 @@
           };
 
           const getRaceBonus = () => {
-            const bonus = props.raceBonuses.find(
-              (item) => item.key === key,
-            )?.value;
+            const bonus = props.raceBonuses.find((item) => item.key === key)
+              ?.value;
 
             if (!bonus && bonus !== 0) {
               return '−';
