96fe8a7feb718abea89b5418d54bd5d4df678ff7
Peterko <peterko@gmail.com>
2024-02-23 08:44:25 +0300

    Fix

--- src/features/SectionHeader.vue
+++ src/features/SectionHeader.vue
@@ -198,7 +198,7 @@
         v-if="onExportFoundry"
         :tooltip="{
           content:
-            '<span>Экспортировать.&nbsp;(<a href=&#34;/info/exporting&#34; target=&#34;_blank&#34;>Инструкция)</a>',
+            '<span>Экспорт.&nbsp;(<a href=&#34;/info/exporting&#34; target=&#34;_blank&#34;>Инструкция)</a>',
         }"
         class="section-header__control is-only-desktop"
         icon="export-foundry"
