afb2d6cb33dfdfa4fff47ef6851541f97c693f30
svifty7 <svifty7@gmail.com>
2024-03-12 03:08:14 +0300

    fix: lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -201,7 +201,7 @@ module.exports = {
       'kebab-case',
       { registeredComponentsOnly: true, ignores: [] },
     ],
-    'vue/component-api-style': ['warn', ['script-setup']],
+    'vue/component-api-style': ['warn', ['script-setup']], // TODO: 'error'
     'vue/block-lang': [
       'warn',
       {
@@ -285,7 +285,7 @@ module.exports = {
       },
     ],
     '@typescript-eslint/consistent-type-assertions': [
-      'warn',
+      'warn', // TODO: 'error'
       { assertionStyle: 'never' },
     ],
   },
