c79669aacdde290959b8dc0c10541ce3ec8f7734
Peterko <peterko@gmail.com>
2024-06-21 22:58:39 +0300

    chekbox

--- src/core/config/themeOverrides.ts
+++ src/core/config/themeOverrides.ts
@@ -3,12 +3,29 @@ import type { GlobalThemeOverrides } from 'naive-ui';
 export const themeOverrides: GlobalThemeOverrides = {
   common: {
     borderRadius: '8px',
+    textColorBase: 'var(--text-color)',
   },
   Checkbox: {
     borderRadius: '4px',
+    color: 'var(--checkbox-bg)',
+    colorDisabled: 'var(--border)',
+    colorChecked: 'var(--primary)',
+    colorDisabledChecked: 'var(--primary-disabled)',
+    textColor: 'var(--text-color)',
+    textColorDisabled: 'var(--text-color-disabled)',
+    border: 'var(--border)',
+    borderChecked: 'var(--border)',
+    borderFocus: 'var(--border)',
+    borderDisabled: 'var(--border)',
+    borderDisabledChecked: 'var(--border)',
+    boxShadowFocus: 'var(--checkbox-shadow)',
   },
   Skeleton: {
     color: 'var(--hover)',
     colorEnd: 'var(--bg-main)',
+  },
+  Button: {
+    colorPrimary: 'var(--primary)',
+    colorPrimaryHover: 'var(--primary-hover)',
   },
 };
