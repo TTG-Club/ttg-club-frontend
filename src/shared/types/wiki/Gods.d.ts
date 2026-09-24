fb20cadd247e148e3b8238b4ec498f29c8fa56db
Vitalii Ungurean <gm.magistr@gmail.com>
2026-09-24 20:21:54 +0300

    Добавить редактирование богов

--- src/shared/types/wiki/Gods.d.ts
+++ src/shared/types/wiki/Gods.d.ts
@@ -1,4 +1,44 @@
+import type { TName, TSource } from '@/shared/types/BaseApiFields';
+
 export enum GodsFilterDefaults {
   dbName = 'gods',
   url = '/filters/gods',
+}
+
+export type GodSex = 'MALE' | 'FEMALE' | 'PHILOSOPHY' | 'UNDEFINE';
+
+export interface GodDetail {
+  name: TName;
+  alignment: string;
+  shortAlignment: string;
+  description?: string;
+  rank?: string;
+  titles?: string[];
+  symbol?: string;
+  domains?: string[];
+  panteons?: string[];
+  images?: string[];
+  source?: TSource;
+}
+
+export interface GodSave {
+  name: string;
+  englishName: string;
+  altName?: string;
+  commitment?: string;
+  sex: GodSex;
+  rank: string;
+  alignment: string;
+  description?: string;
+  alternativeDescription?: string;
+  symbol?: string;
+  nicknames?: string;
+  domains: string[];
+  pantheonId: number;
+  source?: string;
+  page?: number;
+}
+
+export interface GodEdit extends GodSave {
+  id: number;
 }
